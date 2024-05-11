f318e89decb2900a7edd9e6f49ab43da38a3d906
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2024-05-11 23:23:23 +0200

    login implementado

--- app/src/main/java/com/example/appproyectofindegradofranciscodasilva/domain/services/CredentialServices.kt
+++ app/src/main/java/com/example/appproyectofindegradofranciscodasilva/domain/services/CredentialServices.kt
@@ -1,13 +1,11 @@
 package com.example.appproyectofindegradofranciscodasilva.domain.services
 
-import android.util.Log
 import com.example.appproyectofindegradofranciscodasilva.data.model.CredentialRequest
 import com.example.appproyectofindegradofranciscodasilva.data.model.LoginInfoResponse
 import com.example.appproyectofindegradofranciscodasilva.data.repositories.CredentialRepository
 import com.example.appproyectofindegradofranciscodasilva.utils.NetworkResultt
 import com.example.appproyectofindegradofranciscodasilva.utils.TokenManager
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class CredentialServices @Inject constructor(
