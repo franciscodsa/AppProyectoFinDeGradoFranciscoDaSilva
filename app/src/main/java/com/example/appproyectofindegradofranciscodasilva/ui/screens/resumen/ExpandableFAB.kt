2c4d981549de35d87c9bd7f4db56c038e433426d
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2024-05-22 10:25:54 +0200

    ExpandableFAB.kt titulos de opciones con fondo

--- app/src/main/java/com/example/appproyectofindegradofranciscodasilva/ui/screens/resumen/ExpandableFAB.kt
+++ app/src/main/java/com/example/appproyectofindegradofranciscodasilva/ui/screens/resumen/ExpandableFAB.kt
@@ -3,12 +3,14 @@ package com.example.appproyectofindegradofranciscodasilva.ui.screens.resumen
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
@@ -20,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,15 +38,19 @@ fun ExpandableFloatingActionButton(
             enter = expandVertically(),
             exit = shrinkVertically()
         ) {
-            Column(horizontalAlignment = Alignment.End) {
+            Column(
+                horizontalAlignment = Alignment.End
+            ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.padding(8.dp)
                 ) {
                     Text(
+                        modifier = Modifier.clip(RoundedCornerShape(8.dp))
+                            .background(MaterialTheme.colorScheme.primary.copy(alpha = 1f)).padding(8.dp),
                         text = "Clientes",
                         style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurface
+                        color = MaterialTheme.colorScheme.onPrimary
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     FloatingActionButton(
@@ -60,9 +67,11 @@ fun ExpandableFloatingActionButton(
                     modifier = Modifier.padding(8.dp)
                 ) {
                     Text(
+                        modifier = Modifier.clip(RoundedCornerShape(8.dp))
+                            .background(MaterialTheme.colorScheme.primary.copy(alpha = 1f)).padding(8.dp),
                         text = "Contadores",
                         style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurface
+                        color = MaterialTheme.colorScheme.onPrimary
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     FloatingActionButton(
