c0663776d1691236d95ef4c75ae76bd74b36f840
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2024-06-01 18:16:19 +0200

    agrega credenciales a firestore

--- app/src/main/java/com/example/appproyectofindegradofranciscodasilva/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/appproyectofindegradofranciscodasilva/ui/screens/login/LoginViewModel.kt
@@ -5,19 +5,24 @@ import androidx.lifecycle.viewModelScope
 import com.example.appproyectofindegradofranciscodasilva.common.Constantes
 import com.example.appproyectofindegradofranciscodasilva.data.model.CredentialRequest
 import com.example.appproyectofindegradofranciscodasilva.domain.services.CredentialServices
+import com.example.appproyectofindegradofranciscodasilva.domain.services.FirebaseService
 import com.example.appproyectofindegradofranciscodasilva.utils.NetworkResultt
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val credentialServices: CredentialServices
+    private val credentialServices: CredentialServices,
+    private val firebaseService: FirebaseService
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(LoginState())
@@ -48,6 +53,7 @@ class LoginViewModel @Inject constructor(
         }
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     private fun login() {
         if (_uiState.value.email.isEmpty() || _uiState.value.password.isEmpty()) {
             _uiState.update { it.copy(message = Constantes.camposVaciosMessage, isLoading = false) }
@@ -58,6 +64,60 @@ class LoginViewModel @Inject constructor(
                         _uiState.value.email,
                         _uiState.value.password
                     )
+                ).flatMapConcat { credentialResult ->
+                    if (credentialResult is NetworkResultt.Success) {
+                        firebaseService.loginUser(
+                            _uiState.value.email,
+                            _uiState.value.password
+                        )
+                    } else {
+                        flowOf(credentialResult)
+                    }
+                }.catch { cause ->
+                    _uiState.update {
+                        it.copy(
+                            message = cause.message,
+                            isLoading = false
+                        )
+                    }
+                }.collect { result ->
+                    when (result) {
+                        is NetworkResultt.Error -> _uiState.update {
+                            it.copy(
+                                message = result.message,
+                                isLoading = false
+                            )
+                        }
+
+                        is NetworkResultt.Loading -> _uiState.update { it.copy(isLoading = true) }
+
+                        is NetworkResultt.Success -> _uiState.update {
+                            it.copy(
+                                isLoading = false,
+                                logged = true
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    /*private fun login() {
+        if (_uiState.value.email.isEmpty() || _uiState.value.password.isEmpty()) {
+            _uiState.update { it.copy(message = Constantes.camposVaciosMessage, isLoading = false) }
+        } else {
+            viewModelScope.launch {
+                credentialServices.login(
+                    CredentialRequest(
+                        _uiState.value.email,
+                        _uiState.value.password
+                    )
+                )
+                firebaseService.loginUser(
+                    _uiState.value.email,
+                    _uiState.value.password
                 ).catch(action = { cause ->
                     _uiState.update {
                         it.copy(
@@ -78,8 +138,8 @@ class LoginViewModel @Inject constructor(
 
                         is NetworkResultt.Success -> _uiState.update {
                             it.copy(
-                                logged = true,
-                                isLoading = false
+                                isLoading = false,
+                                logged = true
                             )
                         }
                     }
@@ -88,6 +148,6 @@ class LoginViewModel @Inject constructor(
             }
         }
 
-    }
+    }*/
 
 }
